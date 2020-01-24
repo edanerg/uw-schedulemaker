eafac2490ab85da5f70440ed8fbb64d4a077f8bf
Ana <anaelwan10@gmail.com>
2020-01-23 19:05:14 -0500

    backend api set up, frontend receives message from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // Testing connection with backend
+  const [backendMessage, setBackendMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/courses').then(response => {
+      response.json().then(data => {
+        const { courses } = data;
+        setBackendMessage(courses);
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         {/* ADD stuff here */}
-        test
+        {`Backend Message: ${backendMessage}`}
       </header>
     </div>
   );
