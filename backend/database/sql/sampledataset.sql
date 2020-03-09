3ec0f291f25143e7ae2526857440a3a7cebbb1c4
Sunny Yang <sunnyyang270@gmail.com>
2020-03-09 18:33:47 -0400

    fixed Class query + updated sql files

--- backend/database/sql/sampledataset.sql
+++ backend/database/sql/sampledataset.sql
@@ -1,30 +1,46 @@
 -- Execute once to create sample data
 -- Data: 
 --      * 1 course: CS 348
---      * 2 classes: TTh 10-11:30, TTh 1-2:30
 --      * 1 instructor: He,Xi
+--      * 2 LEC: TTh 10-11:30, TTh 1-2:30
+--      * 2 TUT: M 10-11:30, M 1-2:30. Each tutorial corresponds to the respective LEC
 
 INSERT INTO Course
 VALUES (004417, 'CS', '348', 'Introduction to Database Management', 'The main objective of this course is to introduce students to fundamentals of database technology by studying databases from three viewpoints: those of the database user, the database designer, and the database administrator.');
-
-INSERT INTO Course
-VALUES (004407, 'CS', '436', 'Networks and Distributed Computer Systems', 'An introduction to networks, protocols, and distributed systems.');
 
 INSERT INTO Instructor (name)
 VALUES ('He,Xi');
 
 -- TTh 10-11:30
-INSERT INTO Class (id, subject, catalog_number, units, class_number, class_type, section_number, campus, associated_class, 
-    related_component_1, term, academic_level)
-VALUES (111, 'CS', '348', 0.5, 5794, 'LEC', 001, 'UW U', 1, 101, 1201, 'undergraduate');
+-- LEC
+INSERT INTO Class (class_number, subject, catalog_number, units, class_type, section_number, campus, associated_class, 
+    term, academic_level)
+VALUES (5794, 'CS', '348', 0.5, 'LEC', '001', 'UW U', 1, 1201, 'undergraduate');
 
-INSERT INTO ClassTime (id, class_id, start_time, end_time, weekdays, is_active, building, room)
-VALUES (999, 111, '10:00:00', '11:20:00', 'TTh', TRUE, 'RCH', '207');
+INSERT INTO ClassTime (class_number, start_time, end_time, weekdays, building, room, instructor_id)
+VALUES (5794, '10:00:00', '11:20:00', 'TTh', 'RCH', '207', 1);
+
+-- TUT
+INSERT INTO Class (class_number, subject, catalog_number, units, class_type, section_number, campus, associated_class, 
+    term, academic_level)
+VALUES (5795, 'CS', '348', 0.5, 'TUT', '101', 'UW U', 1, 1201, 'undergraduate');
+
+INSERT INTO ClassTime (class_number, start_time, end_time, weekdays, building, room)
+VALUES (5795, '10:00:00', '11:20:00', 'M', 'RCH', '207');
 
 -- TTh 1-2:30
-INSERT INTO Class (id, subject, catalog_number, units, class_number, class_type, section_number, campus, associated_class, 
-    related_component_1, term, academic_level)
-VALUES (222, 'CS', '436', 0.5, 6354, 'LEC', 002, 'UW U', 2, 101, 1201, 'undergraduate');
+-- LEC
+INSERT INTO Class (class_number, subject, catalog_number, units, class_type, section_number, campus, associated_class, 
+    term, academic_level)
+VALUES (6354, 'CS', '348', 0.5, 'LEC', '002', 'UW U', 2, 1201, 'undergraduate');
 
-INSERT INTO ClassTime (id, class_id, start_time, end_time, weekdays, is_active, building, room)
-VALUES (1010, 222, '13:00:00', '14:20:00', 'TTh', TRUE, 'MC', '4040');
+INSERT INTO ClassTime (class_number, start_time, end_time, weekdays, building, room, instructor_id)
+VALUES (6354, '13:00:00', '14:20:00', 'TTh', 'MC', '4040', 1);
+
+-- TUT
+INSERT INTO Class (class_number, subject, catalog_number, units, class_type, section_number, campus, associated_class, 
+    term, academic_level)
+VALUES (6355, 'CS', '348', 0.5, 'TUT', '102', 'UW U', 2, 1201, 'undergraduate');
+
+INSERT INTO ClassTime (class_number, start_time, end_time, weekdays, building, room)
+VALUES (6355, '13:00:00', '14:20:00', 'M', 'MC', '4040');
